f3b010f5ccf0ecc3dc902b0a5b59065aa3462cde
serra <cserra2003@gmail.com>
2024-11-23 02:56:19 +0300

    HAFTA -2

--- Program.cs
+++ Program.cs
@@ -1,70 +1,101 @@
-﻿//HAFTA-1
-//İki sayinin farkini bulan c# kodu
+﻿//HAFTA 2 ÖDEV
 
-//int sayi1, sayi2,fark;
-//Console.WriteLine("Birinci Sayıyı Giriniz: ");
-//sayi1 = Convert.ToInt32(Console.ReadLine());
-//Console.WriteLine("İkinci Sayıyı Giriniz: ");
-//sayi2 = Convert.ToInt32(Console.ReadLine());
-//fark = sayi1 - sayi2;
-//Console.WriteLine("Sayıların farkı:" + fark);
+//Girilen sayının çift - tek kontrolü (if)
+//int sayi;
+//Console.WriteLine("SAYIYI GİRİNİZ:");
+//sayi = Convert.ToInt32(Console.ReadLine());
+
+//if (sayi % 2 == 0)
+//{
+//    Console.WriteLine("SAYI ÇİFTTİR.");
+//}
+//else
+//{
+//    Console.WriteLine("SAYI TEKTİR.");
+//}
 //Console.ReadKey();
 
 
-//5 sayısının karesini bulan c# kodu
 
-//int sayi1, sayinin_karesi;
-//sayi1 = 5;
-//sayinin_karesi = 5 * 5;
-//Console.WriteLine( "5 sayısının karesi:" + sayinin_karesi);
+//Kullanıcı adı admin ve şifre Admin_32453@ olduğunda giriş başarılı değilse başarısız uyarısı veren c# kodu
+
+////string isim,sifre;
+////Console.WriteLine("KULLANICI ADINIZI GİRİNİZ:");
+////isim = Console.ReadLine();
+
+////Console.WriteLine("ŞİFRENİZİ GİRİNİZ:");
+////sifre = Console.ReadLine();
+
+////if (sifre == "Admin_32453@")
+////{
+////    Console.WriteLine("GİRİŞ BAŞARILI!");
+////}
+////else
+////{
+////    Console.WriteLine("GİRİŞ BAŞARISIZ!");
+////}
+////Console.ReadKey();
+///
+
+
+
+
+//10-20,20-30,30-40 arası sıcaklık durumlarına göre ekrana (normal - sıcak - çok sıcak) uyarısı veren c# kodu
+
+//int sicaklik;
+//Console.WriteLine("HAVANIN KAÇ DERECE OLDUĞUNU GİRİNİZ:");
+//sicaklik = Convert.ToInt32(Console.ReadLine());
+
+//if(10<sicaklik && sicaklik < 20)
+//{
+//    Console.WriteLine("NORMAL");
+//}
+//else if(20<=sicaklik && sicaklik<30)
+//    {
+//    Console.WriteLine("SICAK");
+//}
+//else if (30<=sicaklik && sicaklik<40)
+//        {
+//    Console.WriteLine("ÇOK SICAK");
+//}
+//else
+//{
+//    Console.WriteLine("YANLIŞ DEĞER GİRDİNİZ");
+//}
 //Console.ReadKey();
 
-//10 sayisinin 3e bölümünden kalanı bulan c# kodu
 
-//int sayi1, kalan;
-//sayi1 = 10;
-//kalan = 10 % 3;
-//Console.WriteLine("10 sayısının 3'e bölümünden kalan:" + kalan);
-//Console.ReadKey();
+//1 den 10 a kadar sayıları ekrana yazı değeri olarak yazma işlemi (örn 1 girildiğinde ekrana BİR yazılsın - switch yapısı)
 
-//Klavyeden girilen dört sayinin toplamini ve carpimini bulan c# kodu
-//int sayi1, sayi2, sayi3, sayi4, toplam, carpim;
-//Console.WriteLine("Birinci sayıyı giriniz: ");
-//sayi1 = Convert.ToInt32(Console.ReadLine());
-//Console.WriteLine("İkinci sayıyı giriniz: ");
-//sayi2 = Convert.ToInt32(Console.ReadLine());
-//Console.WriteLine("Üçüncü sayıyı giriniz: ");
-//sayi3 = Convert.ToInt32(Console.ReadLine());
-//Console.WriteLine("Dördüncü sayıyı giriniz: ");
-//sayi4 = Convert.ToInt32(Console.ReadLine());
-//toplam = sayi1 + sayi2 + sayi3 + sayi4;
-//Console.WriteLine("Sayıların toplamı:" + toplam);
-//carpim = sayi1 * sayi2 * sayi3 * sayi4;
-//Console.WriteLine("Sayıların çarpımı:" + carpim);
-//Console.ReadKey();
+int sayi;
+Console.WriteLine("1 ile 10 (10 dahil) arası bir sayı giriniz:");
+sayi = Convert.ToInt16(Console.ReadLine());
 
-
-//Klavyeden girilen iki sayinin bölümünü bulan c# kodu
-//int sayi1, sayi2, bolum ;
-//Console.WriteLine("Birinci sayıyı giriniz: ");
-//sayi1 = Convert.ToInt32(Console.ReadLine());
-//Console.WriteLine("İkinci sayıyı giriniz:");
-//sayi2 = Convert.ToInt32(Console.ReadLine());
-//bolum = sayi1 / sayi2;
-//Console.WriteLine("Bölüm:" + bolum);
-//Console.ReadKey();
-
-
-//Klavyeden kenarları girilen dikdörtgenler prizmasının hacmini hesaplayan c# kodu 
-int kenar1, kenar2, kenar3,hacim;
-Console.WriteLine("1.KENARI GİRİNİZ:");
-kenar1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("2. KENARI GİRİNİZ:");
-kenar2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("3. KENARI GİRİNİZ:");
-kenar3 = Convert.ToInt32(Console.ReadLine());
-hacim = kenar1 * kenar2 * kenar3;
-Console.WriteLine("DİKDÖRTGENİN HACMİ: " + hacim);
+switch(sayi)
+{
+    case 1:Console.WriteLine("BİR");
+        break;
+    case 2:Console.WriteLine("İKİ");
+        break;
+    case 3:Console.WriteLine("ÜÇ");
+        break;
+    case 4:Console.WriteLine("DÖRT");
+        break;
+    case 5:Console.WriteLine("BEŞ");
+        break;
+    case 6:Console.WriteLine("ALTI");
+        break;
+    case 7:Console.WriteLine("YEDİ");
+        break;
+    case 8:Console.WriteLine("SEKİZ");
+        break;
+    case 9:Console.WriteLine("DOKUZ");
+        break;
+    case 10:Console.WriteLine("ON");
+        break;
+    default:Console.WriteLine("GİRDİĞİNİZ SAYI BU ARALIKTA DEĞİL.");
+        break;
+}
 Console.ReadKey();
 
 
