784d37c2432413966360773a9c56412ae63c38ba
serra <cserra2003@gmail.com>
2025-01-10 23:55:34 +0300

    Add files via upload

---
+++ HAFTA-1.cs
@@ -0,0 +1,70 @@
+﻿//HAFTA-1
+//İki sayinin farkini bulan c# kodu
+
+//int sayi1, sayi2,fark;
+//Console.WriteLine("Birinci Sayıyı Giriniz: ");
+//sayi1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("İkinci Sayıyı Giriniz: ");
+//sayi2 = Convert.ToInt32(Console.ReadLine());
+//fark = sayi1 - sayi2;
+//Console.WriteLine("Sayıların farkı:" + fark);
+//Console.ReadKey();
+
+
+//5 sayısının karesini bulan c# kodu
+
+//int sayi1, sayinin_karesi;
+//sayi1 = 5;
+//sayinin_karesi = 5 * 5;
+//Console.WriteLine( "5 sayısının karesi:" + sayinin_karesi);
+//Console.ReadKey();
+
+//10 sayisinin 3e bölümünden kalanı bulan c# kodu
+
+//int sayi1, kalan;
+//sayi1 = 10;
+//kalan = 10 % 3;
+//Console.WriteLine("10 sayısının 3'e bölümünden kalan:" + kalan);
+//Console.ReadKey();
+
+//Klavyeden girilen dört sayinin toplamini ve carpimini bulan c# kodu
+//int sayi1, sayi2, sayi3, sayi4, toplam, carpim;
+//Console.WriteLine("Birinci sayıyı giriniz: ");
+//sayi1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("İkinci sayıyı giriniz: ");
+//sayi2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Üçüncü sayıyı giriniz: ");
+//sayi3 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Dördüncü sayıyı giriniz: ");
+//sayi4 = Convert.ToInt32(Console.ReadLine());
+//toplam = sayi1 + sayi2 + sayi3 + sayi4;
+//Console.WriteLine("Sayıların toplamı:" + toplam);
+//carpim = sayi1 * sayi2 * sayi3 * sayi4;
+//Console.WriteLine("Sayıların çarpımı:" + carpim);
+//Console.ReadKey();
+
+
+//Klavyeden girilen iki sayinin bölümünü bulan c# kodu
+//int sayi1, sayi2, bolum ;
+//Console.WriteLine("Birinci sayıyı giriniz: ");
+//sayi1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("İkinci sayıyı giriniz:");
+//sayi2 = Convert.ToInt32(Console.ReadLine());
+//bolum = sayi1 / sayi2;
+//Console.WriteLine("Bölüm:" + bolum);
+//Console.ReadKey();
+
+
+//Klavyeden kenarları girilen dikdörtgenler prizmasının hacmini hesaplayan c# kodu 
+//int kenar1, kenar2, kenar3,hacim;
+//Console.WriteLine("1.KENARI GİRİNİZ:");
+//kenar1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("2. KENARI GİRİNİZ:");
+//kenar2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("3. KENARI GİRİNİZ:");
+//kenar3 = Convert.ToInt32(Console.ReadLine());
+//hacim = kenar1 * kenar2 * kenar3;
+//Console.WriteLine("DİKDÖRTGENİN HACMİ: " + hacim);
+//Console.ReadKey();
+
+
