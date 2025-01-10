233f08ad907219eac9f49a87c43c1f6e92695cd0
serra <cserra2003@gmail.com>
2025-01-10 23:57:23 +0300

    Add files via upload

---
+++ hafta 2.cs
@@ -0,0 +1,101 @@
+﻿//HAFTA 2 ÖDEV
+
+//Girilen sayının çift - tek kontrolü (if)
+//int sayi;
+//Console.WriteLine("SAYIYI GİRİNİZ:");
+//sayi = Convert.ToInt32(Console.ReadLine());
+
+//if (sayi % 2 == 0)
+//{
+//    Console.WriteLine("SAYI ÇİFTTİR.");
+//}
+//else
+//{
+//    Console.WriteLine("SAYI TEKTİR.");
+//}
+//Console.ReadKey();
+
+
+
+//Kullanıcı adı admin ve şifre Admin_32453@ olduğunda giriş başarılı değilse başarısız uyarısı veren c# kodu
+
+////string isim,sifre;
+////Console.WriteLine("KULLANICI ADINIZI GİRİNİZ:");
+////isim = Console.ReadLine();
+
+////Console.WriteLine("ŞİFRENİZİ GİRİNİZ:");
+////sifre = Console.ReadLine();
+
+////if (sifre == "Admin_32453@")
+////{
+////    Console.WriteLine("GİRİŞ BAŞARILI!");
+////}
+////else
+////{
+////    Console.WriteLine("GİRİŞ BAŞARISIZ!");
+////}
+////Console.ReadKey();
+///
+
+
+
+
+//10-20,20-30,30-40 arası sıcaklık durumlarına göre ekrana (normal - sıcak - çok sıcak) uyarısı veren c# kodu
+
+//int sicaklik;
+//Console.WriteLine("HAVANIN KAÇ DERECE OLDUĞUNU GİRİNİZ:");
+//sicaklik = Convert.ToInt32(Console.ReadLine());
+
+//if(10<sicaklik && sicaklik < 20)
+//{
+//    Console.WriteLine("NORMAL");
+//}
+//else if(20<=sicaklik && sicaklik<30)
+//    {
+//    Console.WriteLine("SICAK");
+//}
+//else if (30<=sicaklik && sicaklik<40)
+//        {
+//    Console.WriteLine("ÇOK SICAK");
+//}
+//else
+//{
+//    Console.WriteLine("YANLIŞ DEĞER GİRDİNİZ");
+//}
+//Console.ReadKey();
+
+
+//1 den 10 a kadar sayıları ekrana yazı değeri olarak yazma işlemi (örn 1 girildiğinde ekrana BİR yazılsın - switch yapısı)
+
+//int sayi;
+//Console.WriteLine("1 ile 10 (10 dahil) arası bir sayı giriniz:");
+//sayi = Convert.ToInt16(Console.ReadLine());
+
+//switch(sayi)
+//{
+//    case 1:Console.WriteLine("BİR");
+//        break;
+//    case 2:Console.WriteLine("İKİ");
+//        break;
+//    case 3:Console.WriteLine("ÜÇ");
+//        break;
+//    case 4:Console.WriteLine("DÖRT");
+//        break;
+//    case 5:Console.WriteLine("BEŞ");
+//        break;
+//    case 6:Console.WriteLine("ALTI");
+//        break;
+//    case 7:Console.WriteLine("YEDİ");
+//        break;
+//    case 8:Console.WriteLine("SEKİZ");
+//        break;
+//    case 9:Console.WriteLine("DOKUZ");
+//        break;
+//    case 10:Console.WriteLine("ON");
+//        break;
+//    default:Console.WriteLine("GİRDİĞİNİZ SAYI BU ARALIKTA DEĞİL.");
+//        break;
+//}
+//Console.ReadKey();
+
+
